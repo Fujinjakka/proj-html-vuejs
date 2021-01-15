73824eeb0e27802b284e723ad08223d8b7cd929b
Fujinjakka <calvieri.marco@gmail.com>
2021-01-15 16:58:34 +0100

    All complete (servirebbe piu tempo per vue)

---
+++ js/script.js
@@ -0,0 +1,81 @@
+var app = new Vue(
+  {
+  el: "#vue_container",
+    data: {
+      categories: [
+        {
+          name: "gadget",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "photography",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "lifestyle",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "fashion",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "recipes",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "travel",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "business",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "architecture",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "reviews",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "sports",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "video",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "technology",
+          url: "https://www.pixar.com/404"
+        },
+        {
+          name: "questa no",
+          url: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/GTYSdDW/videoblocks-foggy-sinister-decaying-spot-lit-skull-background-loop_riyik2ui_thumbnail-1080_01.png"
+        }
+      ],
+      isActive: true,
+      on1: false,
+      on2: false,
+      on3: false,
+      on4: false,
+      on5: false,
+      on6: false,
+    },
+  //   methods: {
+  //   // mouseover: function(){
+  //   //   this.class = 'show_text'
+  //   // },
+  //   // mouseleave: function(){
+  //   //   this.class = 'hidden_text'
+  //   // }
+  //   //   toggleClass: function(event){
+  //   //   this.isActive = !this.isActive;
+  //   //   }
+  //   // }
+  // }
+});
+
+// v-bind:class="[isActive ? 'hidden_text' : 'show_text']"
